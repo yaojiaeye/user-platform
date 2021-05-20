5bb91ed2350087e3876ce7a9abcd3ff1ad8b7305
yaojiaiteye@163.com <yaojiaiteye@163.com>
2021-05-20 16:33:12 +0800

    提交App文件信息

--- user-web/src/main/java/com/yaojia/function/app/App.java
+++ user-web/src/main/java/com/yaojia/function/app/App.java
@@ -5,4 +5,7 @@ package com.yaojia.function.app;
  * @Create: 2021/5/20
  */
 public class App {
+    public static void main(String[] args) {
+        System.out.println("ccccccccccccccccccccccccccccccc");
+    }
 }
