19be060acd76bff38f334d27709f3631a20eb31c
yaojiaiteye@163.com <yaojiaiteye@163.com>
2021-05-24 16:49:37 +0800

    测试类信息

--- my-cache/src/main/java/com/yaojia/cache/Cache.java
+++ my-cache/src/main/java/com/yaojia/cache/Cache.java
@@ -6,5 +6,6 @@ package com.yaojia.cache;
 public class Cache {
     public static void main(String[] args) {
         System.out.println("eeeeeeeeeeeeeeeeeeeeeeeeCCCCC");
+        System.out.println("eeeeeeeeeeeeeeeeeeeeeeeennnnnn");
     }
 }
